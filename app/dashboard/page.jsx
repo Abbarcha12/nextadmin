e59cce611644fe2cce691399703ffeab661408ff
Barcha1 <abbarcha123456@gmail.com>
2023-11-11 01:44:17 +0500

    ds

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Card from "../ui/dashboard/card/Card";
+import Charts from "../ui/dashboard/charts/charts";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/Rightbar";
+import Transactions from "../ui/dashboard/Transactions/Transactions";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+      
+            <Card   />
+            <Card   />
+            <Card   />
 
-export default Dashboard
+        
+        </div>
+        <Transactions />
+        <Charts />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
 
-const UsersPage = () => {
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const UsersPage = async () => {
+ 
   return (
-    <div>UsersPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+        
+            <tr >
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={ "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  nae
+                </div>
+              </td>
+              <td>test@gmail.com</td>
+              <td>user Created at</td>
+              <td>admin</td>
+              <td>passive</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form >
+                    <input type="hidden" name="id"  />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+         
+        </tbody>
+      </table>
+      <Pagination/>
+    </div>
+  );
+};
 
-export default UsersPage
+export default UsersPage;
